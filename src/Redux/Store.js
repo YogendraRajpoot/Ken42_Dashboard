8ef66d7a0cf29f6c5d89157eeca525ca35fc6820
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-06-09 11:09:32 +0530

    make it better

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -3,11 +3,10 @@ import thunk from "redux-thunk";
 import { authReducer } from "./auth/reducer";
 import { studentReducer } from "./Student/reducer";
 
-
 const rootReducer = combineReducers({
   auth: authReducer,
-  student: studentReducer
-})
+  student: studentReducer,
+});
 // const Middleware = (store) => (next) => (action) => {
 //   // console.log("Middleware", action, next, store);
 //   return typeof action === "function" ? action(store.dispatch) : next(action)
